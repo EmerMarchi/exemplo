216138aa21802bd1f9560160a3dc5c5f506c09bd
gustavomullersouza <gustavomullerdesouza@gmail.com>
2025-09-24 19:18:49 -0300

    aula 24/09

---
+++ src/main/kotlin/controller/PessoaController.kt
@@ -0,0 +1,24 @@
+package controller
+
+import database.PessoaRepository
+import model.Pessoa
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+//A anotação RestController não pode usar VIEW, basicamente é para construir APIs
+//A anotação Controller permite ter VIEW, para construir Web Applications
+@RestController
+@RequestMapping("/pessoa")
+class PessoaController(
+    val pessoaRepository : PessoaRepository
+){
+    @PostMapping
+    fun cadastrarPessoa(@RequestBody pessoa: Pessoa)
+    : ResponseEntity<Pessoa>{
+        return ResponseEntity.ok(
+            pessoaRepository.save<Pessoa>(pessoa))
+    }
+}
