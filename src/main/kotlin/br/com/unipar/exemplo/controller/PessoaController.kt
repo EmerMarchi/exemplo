836755f1a4d44479065b2e862e99c313c3c2925e
gustavomullersouza <gustavomullerdesouza@gmail.com>
2025-10-21 20:11:02 -0300

    aula 21/10

--- src/main/kotlin/br/com/unipar/exemplo/controller/PessoaController.kt
+++ src/main/kotlin/br/com/unipar/exemplo/controller/PessoaController.kt
@@ -3,6 +3,7 @@ package br.com.unipar.exemplo.controller
 import br.com.unipar.exemplo.database.PessoaRepository
 import br.com.unipar.exemplo.model.Pessoa
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController
 
 //A anotação RestController não pode usar VIEW, basicamente é para construir APIs
 //A anotação Controller permite ter VIEW, para construir Web Applications
+@CrossOrigin(origins = ["http://localhost:5173"])
 @RestController
 @RequestMapping("/pessoa")
 class PessoaController(
