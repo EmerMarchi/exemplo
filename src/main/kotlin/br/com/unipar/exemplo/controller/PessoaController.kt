1f86c4a2c4cf5ccb39bbe00563dcdf3a0efe713f
gustavomullersouza <gustavomullerdesouza@gmail.com>
2025-10-21 19:40:58 -0300

    aula 24/09

--- src/main/kotlin/br/com/unipar/exemplo/controller/PessoaController.kt
+++ src/main/kotlin/br/com/unipar/exemplo/controller/PessoaController.kt
@@ -3,8 +3,11 @@ package br.com.unipar.exemplo.controller
 import br.com.unipar.exemplo.database.PessoaRepository
 import br.com.unipar.exemplo.model.Pessoa
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -24,7 +27,41 @@ class PessoaController(
     }
 
     @GetMapping
-    fun listarPessoas() : List<Pessoa>{
-        return pessoaRepository.findAll()
+    fun buscarPessoas() : ResponseEntity<List<Pessoa>>{
+        return ResponseEntity.ok(pessoaRepository.findAll())
     }
-}
+
+    @GetMapping("/{id}")
+    fun buscarId(@PathVariable id : Long) : ResponseEntity<Pessoa>{
+         val pessoa = pessoaRepository.findById(id)
+        return if (pessoa.isPresent){
+            ResponseEntity.ok(pessoa.get())
+        } else{
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    fun excluirPessoa(@PathVariable id : Long): ResponseEntity<Void>{
+        return if (pessoaRepository.existsById(id)){
+            ResponseEntity.noContent().build()
+        } else{
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @PutMapping("/{id}")
+    fun atualizarPessoa(@PathVariable id: Long, @RequestBody novaPessoa : Pessoa)
+        : ResponseEntity<Pessoa>{
+            return pessoaRepository.findById(id).map { pessoa ->
+                val pessoaAtualizada = pessoa.copy(
+                    nome = novaPessoa.nome,
+                    tipo = novaPessoa.tipo,
+                    cpf = novaPessoa.cpf
+                )
+                ResponseEntity.ok(pessoaRepository.save(pessoaAtualizada))
+            }.orElse(
+                ResponseEntity.notFound().build()
+            )
+        }
+    }
--- src/main/kotlin/br/com/unipar/exemplo/model/Pessoa.kt
+++ src/main/kotlin/br/com/unipar/exemplo/model/Pessoa.kt
@@ -10,7 +10,7 @@ data class Pessoa ( //Por padrão deixar como data class
     @Id //Toda classe Entity precisa informar o id
     @GeneratedValue(strategy = GenerationType.AUTO)//Os ids precisam saber o tipo de geração de Primary Key
     val id : Long? = null,//Usamos o valor nulo para informar a JPA que novos objetos serão novas linhas no banco
-    val nome : String,
-    val tipo : String,
-    val cpf : String
+    val nome : String = "",
+    val tipo : String = "",
+    val cpf : String = ""
 )
