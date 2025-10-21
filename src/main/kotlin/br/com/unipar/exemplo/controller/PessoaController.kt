a92482e3cf1bba61a60cc3c4ccbd930252e70880
gustavomullersouza <gustavomullerdesouza@gmail.com>
2025-10-21 19:41:44 -0300

    aula 21/10

--- src/main/kotlin/br/com/unipar/exemplo/controller/PessoaController.kt
+++ src/main/kotlin/br/com/unipar/exemplo/controller/PessoaController.kt
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/pessoa")
 class PessoaController(
     private val pessoaRepository : PessoaRepository
+
 ){
     @PostMapping
     fun cadastrarPessoa(@RequestBody pessoa: Pessoa)
